014fbe3418723c8660eaa611d559fe9393cb9cf9
queavos <queavos.dev@gmail.com>
2024-04-03 19:45:04 -0400

    local init3

--- 0403/js/app.js
+++ 0403/js/app.js
@@ -1,8 +1,21 @@
 console.log("asdasd");
 
-document.getElementById("texto").innerHTML="<h3>buenas</h3"
+//document.getElementById("texto").innerHTML="<h3>buenas</h3"
 
 function cargar() {  
-    document.getElementById("carrera").value = "una carrera";
+    document.getElementById("carrera").value = "LASI";
+    document.getElementById("carrera").value = "LASI";
+    document.getElementById("curso").value = 2;
+    document.getElementById("semestre").value = 3;
+    document.getElementById("matcode").value = "sx33";
+    document.getElementById("materia").value = "DW2";
+    document.getElementById("feini").value = "2024-03-03";
+    document.getElementById("fefin").value = "2024-07-03";
+
   }
+  function agregar() {
+        console.log("estoy agregando");
+  }
+
  document.getElementById("carga").addEventListener("click",cargar); 
+ document.getElementById("agregar").addEventListener("click", agregar); 
