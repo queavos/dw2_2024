7101e7bba393310168ceaf727a0873b074f67586
queavos <queavos.dev@gmail.com>
2024-04-03 19:20:05 -0400

    local init2

---
+++ 0403/js/app.js
@@ -0,0 +1,8 @@
+console.log("asdasd");
+
+document.getElementById("texto").innerHTML="<h3>buenas</h3"
+
+function cargar() {  
+    document.getElementById("carrera").value = "una carrera";
+  }
+ document.getElementById("carga").addEventListener("click",cargar); 
